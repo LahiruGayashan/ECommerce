693df5849e64478c59eda63694a31651a0818c84
Lahiru Gayashan <gayashan732@gmail.com>
2022-09-01 00:00:33 +0530

    home _page
    create

--- ecommerce/components/HeroBanner.jsx
+++ ecommerce/components/HeroBanner.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Link from 'next/link'
 
-function HeroBanner() {
+const HeroBanner =()=>{
   return (
-      <div>HeroBanner</div>
+      <div className="hero-banner-container">
+       <div>
+        <p className="beats-solo">Small Text</p>
+        <h3>MID TEXT</h3>
+        <img src="" alt="headPhone" className='hero-banner-image' />
+
+        <div>
+           <Link href="/product/ID">
+            <button type='button' >BUTTON TEXT</button>
+            </Link>
+             <div className="desc">
+              <h5>Description</h5>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quidem.</p>
+
+             </div>
+        </div>
+       </div>
+      </div>
   )
 }
 
--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,20 +1,34 @@
 import React from "react";
+import { client } from "../lib/client";
 import { Product, FooterBanner, HeroBanner } from "../components";
 
-const Home = () => (
+const Home = (product,bannerData) => (
   <div>
     <HeroBanner />
+      {console.log(bannerData)}
+
     <div className="products-heading">
       <h2>Best Selling Products</h2>
       <p>Speakers of many variation</p>
     </div>
 
     <div className="products-container">
-      {["Product 1", "Product 2"].map(
-        (product) => product)}
+      {product?.map((product) => product.name)}
     </div>
-    <FooterBanner />
+    <FooterBanner/>
   </div>
 );
+
+export const getServersideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);  
+
+  return {
+    props: {products, bannerData}
+  }
+}
 
 export default Home;

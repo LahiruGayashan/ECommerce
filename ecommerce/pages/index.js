86b08ebd481be9bff43620328c3b9839a814d798
Lahiru Gayashan <gayashan732@gmail.com>
2022-09-05 20:38:52 +0530

    fix
    fix

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -6,6 +6,8 @@ import { Product, FooterBanner, HeroBanner } from '../components';
 const Home = ({ products, bannerData }) => (
   <div>
     <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
+
+    
     <div className="products-heading">
       <h2>Best Seller Products</h2>
       <p>speaker There are many variations passages</p>
@@ -15,7 +17,7 @@ const Home = ({ products, bannerData }) => (
       {products?.map((product) => <Product key={product._id} product={product} />)}
     </div>
 
-    <FooterBanner footerBanner={bannerData && bannerData[0]} />
+    <FooterBanner />
   </div>
 );
 

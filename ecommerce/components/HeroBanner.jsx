ab188c197a20ab4fb2b6681a16e69817ee2cd4ec
Lahiru Gayashan <gayashan732@gmail.com>
2022-09-08 19:47:19 +0530

    error fix
    Hook_up_sanity

--- ecommerce/components/HeroBanner.jsx
+++ ecommerce/components/HeroBanner.jsx
@@ -1,33 +1,27 @@
 import React from 'react';
 import Link from 'next/link';
 
-import { urlFor } from '../lib/client';
-
-const HeroBanner =({heroBanner})=>{
-  return (
-      <div className="hero-banner-container">
-       <div>
-        <p className="beats-solo">{heroBanner.smallText}</p>
-        <h3>{heroBanner.midText}</h3>
-         <h1>{heroBanner.largeText1}</h1>
-        
-        <img src= {urlFor(heroBanner.image)}
-        alt="headPhone" 
-        className='hero-banner-image' />
-
-        <div>
-           <Link href="/product/ID">
-            <button type='button' >BUTTON TEXT</button>
-            </Link>
-             <div className="desc">
-              <h5>Description</h5>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quidem.</p>
-
-             </div>
-        </div>
-       </div>
+const HeroBanner = ({heroBanner}) => {
+return (
+<div className="hero-banner-container">
+     <div>
+      <p className ="beats-solo">{heroBanner.smallText}</p>
+      <h3>MID TEXT</h3>
+      <img src=""alt="headphones" className="hero-banner-image"/>
+      <div>
+       <Link href="/products/ID">
+        <button type="button" >BUTTON TEXT</button>
+        </Link>
+        <div className="desc">
+          <h5>DESCRIPTION TEXT</h5>
+          <p>discription</p>
+          </div>
       </div>
-  )
+     </div>
+  </div>
+ )
 }
+
+
 
 export default HeroBanner
